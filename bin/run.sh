a9fcdc3c55ff52ae6dc6da45c4f7b09e39628677
mattcaruso <carusomat@gmail.com>
2024-04-30 19:36:15 -0400

    Delay startup for db

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-
-gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=1
+# Railway private networking takes 3 seconds to initialize. Delay start so the db can be found on railway.
+# https://docs.railway.app/guides/private-networking#initialization-time
+sleep 3 && gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=1
