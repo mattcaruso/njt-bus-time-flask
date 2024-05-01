4c9e2723db611c0228e0131876405bca81a7275c
mattcaruso <carusomat@gmail.com>
2024-05-01 17:17:08 -0400

    Add IPv6 binding to gunicorn

--- bin/run.sh
+++ bin/run.sh
@@ -3,4 +3,4 @@
 # Railway private networking takes 3 seconds to initialize. Delay start so the db can be found on railway.
 # https://docs.railway.app/guides/private-networking#initialization-time
 sleep 3
-gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4 --threads=2
+gunicorn wsgi:app --bind '[::]:8080' --log-level=debug --workers=4 --threads=2
