239f7bb44ab5a51bc4b2657117f4fe776eec48e5
mattcaruso <carusomat@gmail.com>
2024-05-01 12:56:59 -0400

    test python location

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # Railway private networking takes 3 seconds to initialize. Delay start so the db can be found on railway.
 # https://docs.railway.app/guides/private-networking#initialization-time
-sleep 3 && flask crontab add && gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4 --threads=2
+sleep 3
+which python
+gunicorn wsgi:app --bind 0.0.0.0:8080 --log-level=debug --workers=4 --threads=2
