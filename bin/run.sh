f398972e005a01c7f7d70f49721161757913cc70
mattcaruso <carusomat@gmail.com>
2024-11-25 13:26:39 -0500

    Change worker lifetime values

--- bin/run.sh
+++ bin/run.sh
@@ -3,4 +3,4 @@
 # Railway private networking takes 3 seconds to initialize. Delay start so the db can be found on railway.
 # https://docs.railway.app/guides/private-networking#initialization-time
 sleep 3
-gunicorn wsgi:app --bind '[::]:8080' --log-level=debug --access-logfile '-' --error-logfile '-' --enable-stdio-inheritance --capture-output --workers=4 --threads=2 --max-requests 1000 --max-requests-jitter 50
+gunicorn wsgi:app --bind '[::]:8080' --log-level=debug --access-logfile '-' --error-logfile '-' --enable-stdio-inheritance --capture-output --workers=4 --threads=2 --max-requests 100 --max-requests-jitter 20
